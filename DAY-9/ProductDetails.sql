f5396379cfb7ee249c36bf322f94918add4bdf61
KanchanMehra <148185899+KanchanMehra@users.noreply.github.com>
2025-11-05 15:59:20 +0530

    Delete DAY-9 directory

--- DAY-9/ProductDetails.sql
+++
@@ -1,9 +0,0 @@
-/*Product details
-The e-commerce offering online services to their clients. They are managing the product's sale and purchase 
-through this website. They wants find the products with different categories like furniture,laptop and mobile */
-
--- Write your query here
-SELECT *
-FROM product
-WHERE category IN ('furniture', 'laptop', 'mobile')
-  AND price < 50000;
